04f02badae7e2d2c5bd929b794f53968cddbe6c6
Mohammed Al-Mahrooqi <mohammed010607@gmail.com>
2026-07-01 13:20:00 +0400

    Commiting task 7, a movie ticket pricing calculator

---
+++ C# Fundamentals Tasks/Task7_MovieTickets/Task7_MovieTickets/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task7_MovieTickets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            double ticketPrice = 0;
+            if(age > 0 && age <= 12)
+            {
+                Console.WriteLine("You are a child: ");
+                Console.WriteLine("Your ticket price is 2.000 OMR");
+            }
+            else if (age > 12 && age <= 59)
+            {
+                Console.WriteLine("You are an adult: ");
+                Console.WriteLine("Your ticket price is 5.000 OMR");
+            }
+            else
+            {
+                Console.WriteLine("You are a senior: ");
+                Console.WriteLine("Your ticket price is 3.000 OMR");
+            }
+        }
+    }
+}
