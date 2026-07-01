e2bc5263fb1daac4f9b70dbc4c95af7e4244a3ed
Mohammed Al-Mahrooqi <mohammed010607@gmail.com>
2026-07-01 13:12:06 +0400

    Commiting Temperature Converter (Task 6)

---
+++ C# Fundamentals Tasks/Task6_TemperatureConversion/Task6_TemperatureConversion/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task6_TemperatureConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a Temperature in Celsius: ");
+            double temp = Convert.ToDouble(Console.ReadLine());
+            double tempConversion = (temp * 9 / 5) + 32;
+            Console.WriteLine("Temperature in Fahrenheit is: " + tempConversion);
+
+            if (temp < 10) {
+                Console.WriteLine("It is Cold");
+            }
+            else if (temp > 10 && temp <= 30) {
+                Console.WriteLine("It is Mild");
+            }
+            else {
+                Console.WriteLine("It is Hot");
+            }
+        }
+    }
+}
