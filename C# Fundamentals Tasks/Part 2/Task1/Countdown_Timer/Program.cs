41f54c652de602174f8e7d07bb63a0db6d2318e5
Mohammed Al-Mahrooqi <mohammed010607@gmail.com>
2026-07-02 11:41:48 +0400

    Task 1 Completion

---
+++ C# Fundamentals Tasks/Part 2/Task1/Countdown_Timer/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Countdown_Timer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an Integer: ");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("Countdown: ");
+
+            for (int i = num; i >= 0; i--) {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
