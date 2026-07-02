d2a70f27d73a75969497b5c44590c12d1e1df679
Mohammed Al-Mahrooqi <mohammed010607@gmail.com>
2026-07-02 16:47:32 +0400

    Task 5, Number Guessing Game

---
+++ C# Fundamentals Tasks/Part 2/Task 5/Number_Guessing_Game/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Number_Guessing_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fixedNum = 42; // The fixed number to guess
+            int guess;
+            int attempts = 0;
+            do
+            {
+                Console.WriteLine("Guess a Number:");
+                guess = int.Parse(Console.ReadLine());
+                attempts++;
+
+                if (guess > fixedNum) { 
+                    Console.WriteLine("Too High Try Again.");
+
+                }
+                else if (guess < fixedNum)
+                {
+                    Console.WriteLine("Too Low Try Again.");
+                }
+                else
+                {
+                    Console.WriteLine("Correct. It took you "+attempts + " attempts.");
+                }
+            }
+            while (guess != fixedNum);
+        }
+    }
+}
