1415368068e48a3577a0c55a2a3a6dfd13f934c4
Mohammed Al-Mahrooqi <mohammed010607@gmail.com>
2026-07-02 22:23:44 +0400

    Consolidate all tasks into a single unified solution

---
+++ C# Fundamentals Tasks/Part 2/Task 6/SafeDivisionCalculator/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace SafeDivisionCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the First Number: ");
+            double num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Second Number: ");
+            double num2 = double.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
