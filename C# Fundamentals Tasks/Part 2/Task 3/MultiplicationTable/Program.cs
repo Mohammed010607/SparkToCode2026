635c2b37a8751fb8965b5bc5a2b4eaa2544b2ddd
Mohammed Al-Mahrooqi <mohammed010607@gmail.com>
2026-07-02 12:37:33 +0400

    Multiplication Table Code

---
+++ C# Fundamentals Tasks/Part 2/Task 3/MultiplicationTable/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace MultiplicationTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an Integer: ");
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++) {
+                int result = num * i;
+                Console.WriteLine(num +" * "+i+" = " + result);
+            }
+        }
+    }
+}
