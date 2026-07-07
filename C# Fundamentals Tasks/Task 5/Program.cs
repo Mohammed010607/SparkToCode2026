36a8cddfd4c2b350135a9b9268c89c41a224e74e
Mohammed Al-Mahrooqi <mohammed010607@gmail.com>
2026-07-07 17:22:42 +0400

    Commiting Task 5, Q6 Code (Filtering Shopping List)

--- C# Fundamentals Tasks/Task 5/Program.cs
+++ C# Fundamentals Tasks/Task 5/Program.cs
@@ -5,7 +5,8 @@
         static void Main(string[] args)
         {
             /*
-            //Task 1 (Fixed Grade Array)
+            //Task 1 (Fixed Grade Array):
+
             int[] grades = new int[5];
             for (int i = 0; i < grades.Length; i++)
             {
@@ -34,7 +35,8 @@
             ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-            // Task 3 (Browsing History Stack)
+            // Task 3 (Browsing History Stack):
+
             Stack<string> history = new Stack<string>();
             Console.WriteLine("Enter 3 Website URLs: ");
             Console.WriteLine("");
@@ -46,11 +48,12 @@
             }
             string prevPage = history.Pop();
             Console.WriteLine("Previous Page is: " + prevPage);
-            */
+          
             ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-            // Task 4 (Customer Service Queue)
+            // Task 4 (Customer Service Queue):
+
             Queue<string> waitingLine = new Queue<string>();
             Console.WriteLine("Enter 3 Customer Nmaes: ");
             Console.WriteLine("");
@@ -60,6 +63,53 @@
                 waitingLine.Enqueue(Console.ReadLine());
             }
             Console.WriteLine("First Customer in Line is: "+ waitingLine.Dequeue());
+            
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            //Task 5 (Array Grade Rang):
+
+            int[] gradeArray = new int[5];
+            Console.WriteLine("Enter Your 5 Grades: ");
+            for(int i = 0; i <gradeArray.Length; i++)
+            {
+                Console.WriteLine("Enter Grade "+(i+1)+": ");
+                gradeArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Array.Sort(gradeArray);
+            int sum = 0;
+            foreach(int mark in gradeArray)
+            {
+                sum += mark;
+            }
+            double average = sum / 5;
+            Console.WriteLine("Lowest Mark: " + gradeArray[0]);
+            Console.WriteLine("Highest Mark: " + gradeArray[gradeArray.Length-1]);
+            Console.WriteLine("Your Agerage Mark is: " + gradeArray.Average());
+            */
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            
+            //Task 6 (Filtering Shopping List)
+            List<string> shoppingList = new List<string>();
+            string stopping = "";
+            stopping.ToLower();
+            Console.WriteLine("Add Your Shopping List Items (type 'done' to finish): ");
+            while(stopping != "done")
+            {
+                if(stopping != "done")
+                {
+                    shoppingList.Add(Console.ReadLine());
+                } 
+                
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Remove One Item From The List. ");
+            string remove = Console.ReadLine();
+            shoppingList.Remove(remove);
+            Console.WriteLine("");
+            Console.WriteLine("The Final Shopping List is: ");
+            foreach (string entry in shoppingList) {
+                Console.WriteLine(entry);
+            }
         }
     }
 }
