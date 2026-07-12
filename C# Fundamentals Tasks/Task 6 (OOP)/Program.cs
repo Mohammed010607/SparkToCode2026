e3489e49fcb062313a198fff1b9baa60faa06881
Mohammed Al-Mahrooqi <mohammed010607@gmail.com>
2026-07-12 12:04:19 +0400

    Code For Deposit Method

---
+++ C# Fundamentals Tasks/Task 6 (OOP)/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task_6__OOP_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    class BankAccount
+    {
+        public int AccountNumber;
+        public string HolderName;
+        public double Balance;
+
+        public void Deposit(double amount) {
+            Balance += amount;
+            SendEmail();
+        }
+
+
+    }
+}
